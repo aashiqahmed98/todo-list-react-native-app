ddb734ebe0ebe548d32472f5672c21a3adc6d561
Aashiq Ahmed M <ahmed.aashiq@saama.com>
2022-10-14 23:36:54 +0530

    setup the app + create header component

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+import Header from './components/Header'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [todos, setTodos] = useState([
+		{
+			text: 'buy coffee',
+			key: 1,
+		},
+		{
+			text: 'create an app',
+			key: 2,
+		},
+		{
+			text: 'cycle',
+			key: 3,
+		},
+	])
+
+	return (
+		<View style={styles.container}>
+			<Header />
+			<View style={styles.content}>
+				{/* to form */}
+				<View style={styles.list}>
+					<FlatList
+						data={todos}
+						renderItem={({ item }) => <Text>{item.text}</Text>}
+						keyExtractor={(item) => item.key}
+					/>
+				</View>
+			</View>
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+	},
+	content: {
+		padding: 40,
+	},
+	list: {
+		marginTop: 20,
+	},
+})
